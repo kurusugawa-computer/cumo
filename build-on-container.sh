8801029366ab182bb82097c265de059befbddeb6
Kotet <kotet0418@gmail.com>
2021-05-17 18:52:38 +0900

    ビルドスクリプトを分割

---
+++ build-on-container.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+WORKDIR="/workdir"
+
+set -xe
+
+cd ${WORKDIR}/client
+yarn
+
+cd "${WORKDIR}"
+PLUGIN_TS=./client/node_modules/.bin/protoc-gen-ts
+protoc \
+    --python_out=./lib/pointcloud_viewer/ \
+    --ts_out=import_style=commonjs,binary:./client/src \
+    --js_out=import_style=commonjs,binary:./client/src \
+    -I. \
+    --plugin=protoc-gen-ts="${PLUGIN_TS}" \
+    protobuf/server.proto
+protoc \
+  --python_out=./lib/pointcloud_viewer/ \
+  --ts_out=import_style=commonjs,binary:./client/src \
+  --js_out=import_style=commonjs,binary:./client/src \
+  -I. \
+  --plugin=protoc-gen-ts="${PLUGIN_TS}" \
+  protobuf/client.proto
+rm -rf ./lib/pointcloud_viewer/_protobuf
+mv ./lib/pointcloud_viewer/protobuf ./lib/pointcloud_viewer/_protobuf
+
+cd ${WORKDIR}/client
+yarn build
+
+cd ${WORKDIR}
+rm -rf ./lib/pointcloud_viewer/public
+cp -R client/public lib/pointcloud_viewer/
+
+cd ${WORKDIR}/lib
+poetry build
--- build.sh
+++ build.sh
@@ -1,46 +1,9 @@
-SCRIPT_DIR=$(
-    cd $(dirname $0)
-    pwd
-)
+#!/bin/bash
 
 DOCKER_IMAGE="pointcloud-viewer-builder"
-
-set -xe
 
 if [[ "$(docker images -q ${DOCKER_IMAGE} 2> /dev/null)" == "" ]]; then
   docker build -t ${DOCKER_IMAGE} - < Dockerfile
 fi
 
-cd ${SCRIPT_DIR}/client
-YARN="docker run --rm -v ${PWD}:/workdir -w /workdir ${DOCKER_IMAGE} yarn"
-${YARN}
-
-cd "${SCRIPT_DIR}"
-PROTOC="docker run --rm -v ${PWD}:/workdir -w /workdir ${DOCKER_IMAGE} protoc"
-PLUGIN_TS=./client/node_modules/.bin/protoc-gen-ts
-${PROTOC} \
-    --python_out=./lib/pointcloud_viewer/ \
-    --ts_out=import_style=commonjs,binary:./client/src \
-    --js_out=import_style=commonjs,binary:./client/src \
-    -I. \
-    --plugin=protoc-gen-ts="${PLUGIN_TS}" \
-    protobuf/server.proto
-${PROTOC} \
-  --python_out=./lib/pointcloud_viewer/ \
-  --ts_out=import_style=commonjs,binary:./client/src \
-  --js_out=import_style=commonjs,binary:./client/src \
-  -I. \
-  --plugin=protoc-gen-ts="${PLUGIN_TS}" \
-  protobuf/client.proto
-docker run --rm -v ${PWD}:/workdir -w /workdir ${DOCKER_IMAGE} mv ./lib/pointcloud_viewer/protobuf ./lib/pointcloud_viewer/_protobuf
-
-cd ${SCRIPT_DIR}/client
-${YARN} build
-
-cd ${SCRIPT_DIR}
-rm -rf ./lib/pointcloud_viewer/public
-cp -R client/public lib/pointcloud_viewer/
-
-cd lib
-POETRY="docker run --rm -v ${PWD}:/workdir -w /workdir ${DOCKER_IMAGE} poetry"
-${POETRY} build
+docker run --rm -v ${PWD}:/workdir -w /workdir ${DOCKER_IMAGE} bash build-on-container.sh
