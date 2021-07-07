59278f6d53b0090638e3e470f78a7fe959cfc2d9
Kotet <kotet0418@gmail.com>
2021-07-07 21:21:14 +0900

    format webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,43 +5,43 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const outputpath = path.resolve(__dirname, 'public');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './src/index.ts',
-    output: {
-        filename: 'bundle.js',
-        path: `${outputpath}`,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.d\.ts/,
-                use: 'ignore-loader',
-                exclude: '/node_modules/',
-            },
-            {
-                test: /\.ts/,
-                use: 'ts-loader',
-                exclude: /node_modules|\.d\.ts/,
-            },
-            {
-                test: /\.css/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader',],
-                exclude: '/node_modules/',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.css'],
-    },
-    devServer: {
-        contentBase: `${outputpath}/`,
-        open: true,
-        hot: true,
-        watchContentBase: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-    ],
-}
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: `${outputpath}`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.d\.ts/,
+        use: 'ignore-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.ts/,
+        use: 'ts-loader',
+        exclude: /node_modules|\.d\.ts/
+      },
+      {
+        test: /\.css/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.css']
+  },
+  devServer: {
+    contentBase: `${outputpath}/`,
+    open: true,
+    hot: true,
+    watchContentBase: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ]
+};
