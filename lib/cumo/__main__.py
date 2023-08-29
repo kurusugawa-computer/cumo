a6990153f3b9dc928108b971d3fa4eec6d53b94c
Kotet <kotet0418@gmail.com>
2023-08-29 18:48:37 +0900

    スクリーンショット取得時にオーバーレイが正しく描画されない問題を修正

--- lib/cumo/__main__.py
+++ lib/cumo/__main__.py
@@ -74,9 +74,9 @@ def main():
             print("saved: "+name)
 
     def take_screenshots():
-
-        for p in [(1, 0, 0, "screenshot_x.png"), (0, 1, 0, "screenshot_y.png"), (0, 0, 1, "screenshot_z.png")]:
+        for p in [(-1, 0, 0, "screenshot_x.png"), (0, -1, 0, "screenshot_y.png"), (0, 0, -1, "screenshot_z.png")]:
             viewer.set_camera_position(p[0], p[1], p[2])
+            viewer.set_camera_target(0, 0, 0)
             viewer.set_camera_roll(0, p[1], p[2], p[0])
             data = viewer.capture_screen()
             save_png(p[3], data)
